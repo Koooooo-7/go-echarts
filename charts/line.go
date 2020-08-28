a5911c76069ff0e8f5f5dec50f2114714685d09e
koooooo-7 <369491420@qq.com>
2020-08-29 00:45:33 +0800

    [remove] remove duplicated commit.

--- charts/line.go
+++ charts/line.go
@@ -37,9 +37,3 @@ func (c *Line) AddSeries(name string, data []opts.LineData, opts ...SeriesOpts) 
 	return c
 }
 
-// Validate validates the given configuration.
-// TODO: add more Line validate cases
-func (c *Line) Validate() {
-	c.XAxisList[0].Data = c.xAxisData
-	c.Assets.Validate(c.AssetsHost)
-}
